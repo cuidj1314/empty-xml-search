cfb595d191e042068c4eb9f64f59d46316c80f04
cuidj <dejun.cui@dhc.com.cn>
2022-03-17 09:21:41 +0800

    modify

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 fun main(args: Array<String>) {
-    println("Hello, World")
+    val readFile = ReadFile();
+    readFile.emptyXmlSearch("D:\\svn\\03_開発庫\\06_本変換\\ソース\\01_ツール変換後ソース")
 }
 
---
+++ src/main/kotlin/org/example/ReadFile.kt
@@ -0,0 +1,24 @@
+package org.example
+
+import org.dom4j.Document
+import org.dom4j.DocumentHelper
+import java.io.File
+
+open class ReadFile {
+
+    fun emptyXmlSearch(inputPath: String) {
+        val fileTree: FileTreeWalk = File(inputPath).walk()
+        fileTree.filter { it.isFile }
+            .filter { it.extension == "xml" }
+            .forEach {
+                try {
+                    val doc: Document = DocumentHelper.parseText(it.readText())
+                    when (doc.rootElement.elements().size) {
+                        0 -> println("Emptyファイル名--------------------------->： ${it.path}")
+                    }
+                } catch (e: Throwable) {
+                    println("異常ファイル--------------------------->： ${it.path}")
+                }
+            }
+    }
+}
