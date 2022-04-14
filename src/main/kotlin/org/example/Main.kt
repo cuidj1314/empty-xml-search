56b9660977dde7e67d083a29ae102b254bfb76ee
cuidj <dejun.cui@dhc.com.cn>
2022-04-14 11:25:35 +0800

    creat

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -2,6 +2,9 @@ package org.example
 
 fun main(args: Array<String>) {
     val readFile = ReadFile();
-    readFile.emptyXmlSearch("D:\\svn\\03_開発庫\\06_本変換\\ソース\\01_ツール変換後ソース")
+    val validateCheck = ValidateCheck();
+//    readFile.emptyXmlSearch("D:\\svn\\03_開発庫\\06_本変換\\ソース\\01_ツール変換後ソース")
+//    readFile.emptyXmlSearch("D:\\svn\\03_開発庫\\06_本変換\\ソース\\02_手修正後ソース")
+    validateCheck.actionPathOutput("D:\\svn\\01_基線庫\\20211227\\20_原本ソース一式\\H\\src\\KJN\\Java\\C0TjApp")
 }
 
---
+++ src/main/kotlin/org/example/ValidateCheck.kt
@@ -0,0 +1,94 @@
+package org.example
+
+import org.dom4j.Document
+import org.dom4j.DocumentHelper
+import java.io.File
+
+class ValidateCheck {
+
+    fun actionPathOutput(inputPath: String) {
+        val fileTree: FileTreeWalk = File(inputPath).walk()
+        fileTree.filter { it.isFile }
+            .filter { it.extension == "xml" }
+            .filter { it.name.toString().contains("struts", ignoreCase = true) }
+            .forEach {
+                try {
+                    val doc: Document = DocumentHelper.parseText(it.readText())
+                    if (doc.rootElement.elements().size > 0) {
+                        doc.rootElement.elements("action-mappings").forEach { x ->
+                            x.elements("action").forEach { y ->
+                                if (y.attribute("validate") != null) {
+                                    if (!y.attribute("path").value.contains("Dispatch", ignoreCase = true)) {
+                                        when (y.attribute("validate").value) {
+                                            "true" -> {
+                                                println(
+                                                    "validate必要--------------------------->： ${
+                                                        y.attribute("path").value.substringAfter(
+                                                            "/"
+                                                        )
+                                                            .replaceFirstChar { m -> if (m.isLowerCase()) m.uppercase() else m.toString() }
+                                                    }Controller.java"
+                                                )
+                                            }
+                                            "false" -> {
+                                                println(
+                                                    "validate必要はない--------------------------->： ${
+                                                        y.attribute("path").value.substringAfter(
+                                                            "/"
+                                                        )
+                                                            .replaceFirstChar { m -> if (m.isLowerCase()) m.uppercase() else m.toString() }
+                                                    }Controller.java"
+                                                )
+                                            }
+                                            else -> {
+                                                println(
+                                                    "確認必要--------------------------->： ${
+                                                        y.attribute("path").value.substringAfter(
+                                                            "/"
+                                                        )
+                                                            .replaceFirstChar { m -> if (m.isLowerCase()) m.uppercase() else m.toString() }
+                                                    }Controller.java"
+                                                )
+                                            }
+                                        }
+                                    } else {
+                                        when (y.attribute("validate").value) {
+                                            "true" -> {
+                                                y.elements("forward").forEach { z ->
+                                                    println(
+                                                        "validate必要--------------------------->： ${
+                                                            z.attribute("path").value.substringAfter(
+                                                                "/"
+                                                            ).substringBefore(".do")
+                                                                .replaceFirstChar { m -> if (m.isLowerCase()) m.uppercase() else m.toString() }
+                                                        }Controller.java"
+                                                    )
+                                                }
+                                            }
+                                            "false" -> {
+                                                y.elements("forward").forEach { z ->
+                                                    println(
+                                                        "validate必要はない--------------------------->： ${
+                                                            z.attribute("path").value.substringAfter(
+                                                                "/"
+                                                            ).substringBefore(".do")
+                                                                .replaceFirstChar { m -> if (m.isLowerCase()) m.uppercase() else m.toString() }
+                                                        }Controller.java"
+                                                    )
+                                                }
+                                            }
+                                            else -> {
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (e: Throwable) {
+                    println("異常ファイル--------------------------->： ${it.path}")
+                }
+            }
+    }
+}
